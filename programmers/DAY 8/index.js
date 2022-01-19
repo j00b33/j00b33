1537bf03a46bb2905d4f2a1322028f9d4cb496b6
j00b33 <pjoobee@gmail.com>
2022-01-19 09:37:36 +0900

    DAY 8-2

--- programmers/DAY 8/index.js
+++ programmers/DAY 8/index.js
@@ -1,6 +1,17 @@
-//문자열이면 거르기
+// 내코드 - 문자열이면 거르기 isNaN 사용함
+function solution(s) {
+    for (let i= 0; i<s.length;i++){
+        if (isNaN(s[i])){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
 
-//약수의 총  합 구하기
+
+// 내코드 - 약수의 총  합 구하기
 function solution(n) {
     let answer = 0;
     for (let i=0; i<n+1; i++){
